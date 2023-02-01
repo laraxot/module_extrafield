157c81543908c25f216dfe7005a79bf082078975
marco76tv <marco.sottana@gmail.com>
2023-02-01 18:26:27 +0100

    .

---
+++ Http/Livewire/Modal/ExtraFields/Group/AssignName.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\ExtraField\Http\Livewire\Modal\ExtraFields\Group;
+
+use Illuminate\Contracts\Support\Renderable;
+use Modules\ExtraField\Models\ExtraFieldGroup;
+use WireElements\Pro\Components\Modal\Modal;
+
+class AssignName extends Modal {
+    public string $group_id;
+    public array $form_data;
+
+    /**
+     * @var array<string, string>
+     */
+    protected $listeners = [
+        // 'update_form_data' => 'updateFormData',
+    ];
+
+    public function mount(string $group_id): void {
+        $this->form_data = [];
+        $this->group_id = $group_id;
+    }
+
+    public function save() {
+        $this->form_data['id'] = $this->group_id;
+        $row = ExtraFieldGroup::firstOrNew($this->form_data);
+        $row->save();
+        // dddx(['model', ExtraFieldGroup::find($this->group_id), 'group_id' => $this->group_id, 'form_data' => $this->form_data]);
+    }
+
+    public function render(): Renderable {
+        /**
+         * @phpstan-var view-string
+         */
+        $view = 'extrafield::livewire.modal.group.assign_name';
+
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view($view, $view_params);
+    }
+
+    public static function attributes(): array {
+        return [
+            // Set the modal size to 2xl, you can choose between:
+            // xs, sm, md, lg, xl, 2xl, 3xl, 4xl, 5xl, 6xl, 7xl
+            'size' => 'xl',
+        ];
+    }
+}
---
+++ Resources/views/livewire/modal/group/assign_name.blade.php
@@ -0,0 +1,17 @@
+<x-wire-elements-pro::bootstrap.modal on-submit="save" :content-padding="false">
+    <x-slot name="title">Che nome vuoi mettere al gruppo?</x-slot>
+
+    <input type="text" class="form-control" name="name" wire:model.lazy="form_data.name">
+
+    <x-slot name="buttons">
+
+        <button type="submit" class="btn btn-primary">
+            Save Changes
+        </button>
+
+
+        <button type="button" class="btn btn-danger" wire:click="$emit('modal.close')">
+            Cancel
+        </button>
+    </x-slot>
+</x-wire-elements-pro::bootstrap.modal>
