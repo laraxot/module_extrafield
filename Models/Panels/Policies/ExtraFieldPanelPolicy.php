db06d76b7f05512097787ca74d58f8855270b1cc
Malebestia <vair81@gmail.com>
2023-02-07 19:48:42 +0100

    up

--- Models/Panels/Policies/ExtraFieldPanelPolicy.php
+++ Models/Panels/Policies/ExtraFieldPanelPolicy.php
@@ -1,13 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\ExtraField\Models\Panels\Policies;
 
-use Illuminate\Auth\Access\HandlesAuthorization;
-use Modules\LU\Models\User as User;
-use Modules\ExtraField\Models\Panels\Policies\ExtraFieldPanelPolicy as Post;
-
+use Modules\Cms\Contracts\PanelContract;
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
+use Modules\LU\Services\ProfileService;
+use Modules\Xot\Contracts\UserContract;
 
-class ExtraFieldPanelPolicy extends XotBasePanelPolicy
-{
+class ExtraFieldPanelPolicy extends XotBasePanelPolicy {
+    public function index(?UserContract $user, PanelContract $panel): bool {
+        return ProfileService::make()->get($user)->hasPermissionTo($panel->getPath().'-'.__FUNCTION__);
+    }
 }
