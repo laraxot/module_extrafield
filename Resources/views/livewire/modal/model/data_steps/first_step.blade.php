67ed79a36102ccf7ff10e6c671a85493e6afb9a0
marco76tv <marco.sottana@gmail.com>
2023-02-07 11:55:59 +0100

    up

--- Resources/views/livewire/modal/model/data_steps/first_step.blade.php
+++ Resources/views/livewire/modal/model/data_steps/first_step.blade.php
@@ -1,5 +1,5 @@
 @extends('ui::layouts.wizard')
 @section('content')
     <x-input.group type="select" name="group_id" :options="$group_opts" class="mb-5" />
-    <button type="button" class="btn btn-primary" wire:click="save()">Save</button>
+    {{-- <button type="button" class="btn btn-primary" wire:click="save()">Save</button> --}}
 @endsection
