dceca0558c5b5d8d879f20e29dfbe6ad2bce39e5
marco76tv <marco.sottana@gmail.com>
2023-02-07 12:34:39 +0100

    up

--- Http/Livewire/Modal/ExtraFields/AddData.php
+++ Http/Livewire/Modal/ExtraFields/AddData.php
@@ -49,6 +49,19 @@ class AddData extends Modal {
         return view($view, $view_params);
     }
 
+    public static function behavior(): array {
+        return [
+            // Close the modal if the escape key is pressed
+            'close-on-escape' => true,
+            // Close the modal if someone clicks outside the modal
+            'close-on-backdrop-click' => false,
+            // Trap the users focus inside the modal (e.g. input autofocus and going back and forth between input fields)
+            'trap-focus' => true,
+            // Remove all unsaved changes once someone closes the modal
+            'remove-state-on-close' => false,
+        ];
+    }
+
     public static function attributes(): array {
         return [
             // Set the modal size to 2xl, you can choose between:
