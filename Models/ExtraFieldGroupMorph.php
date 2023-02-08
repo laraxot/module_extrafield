6b671aa4075f451ce15c9a5302c860ba3d9f0af3
marco76tv <marco.sottana@gmail.com>
2023-02-08 19:20:00 +0100

    up

---
+++ Models/ExtraFieldGroupMorph.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\ExtraField\Models;
+
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Modules\ExtraField\Models\ExtraFieldMorph.
+ *
+ * @property int                             $id
+ * @property string|null                     $model_type
+ * @property int|null                        $model_id
+ * @property int|null                        $user_id
+ * @property string|null                     $created_by
+ * @property string|null                     $updated_by
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int|null                        $extra_field_id
+ * @property string|null                     $value
+ * @property string|null                     $value_class
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereExtraFieldId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereModelId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereModelType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereValue($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ExtraFieldMorph whereValueClass($value)
+ *
+ * @mixin \Eloquent
+ */
+class ExtraFieldGroupMorph extends BaseMorphPivot {
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'id',
+        'model_id',
+        'model_type',
+        'user_id',
+        'value',
+        'value_class',
+        'uuid',
+        'extra_field_id',
+    ];
+
+    public function extraFieldMorphUserValues(): HasMany {
+        return $this->hasMany(ExtraFieldMorphUserValue::class);
+    }
+
+    public function extraFieldMorphUserValue(): HasOne {
+        return $this->hasOne(ExtraFieldMorphUserValue::class)
+            ->where('user_id', Auth::id())
+        ;
+    }
+
+    public function userValue(string $user_id) {
+        $res = ExtraFieldMorph::firstOrNew([
+            'user_id' => $user_id,
+            'model_type' => $this->model_type,
+            'model_id' => $this->model_id,
+            'extra_field_id' => $this->extra_field_id,
+            'uuid' => $this->uuid,
+        ]);
+
+        return $res->value;
+    }
+
+    public function updateUserValue(string $user_id, $value) {
+        // si creano dei doppioni con update. perchè?
+        $row = ExtraFieldMorph::firstOrCreate([
+            'user_id' => $user_id,
+            'model_type' => $this->model_type,
+            'model_id' => $this->model_id,
+            'extra_field_id' => $this->extra_field_id,
+            'uuid' => $this->uuid,
+        ]);
+        $res = tap($row)->update(['value' => $value]);
+
+        return $res;
+    }
+}
--- Models/Traits/HasExtraFields.php
+++ Models/Traits/HasExtraFields.php
@@ -9,14 +9,13 @@ namespace Modules\ExtraField\Models\Traits;
 
 use Modules\ExtraField\Models\ExtraField;
 use Modules\ExtraField\Models\ExtraFieldGroup;
+use Modules\ExtraField\Models\ExtraFieldGroupMorph;
 use Modules\ExtraField\Models\ExtraFieldMorph;
 
-trait HasExtraFields
-{
+trait HasExtraFields {
     use \Staudenmeir\EloquentHasManyDeep\HasRelationships;
 
-    public function extraFields()
-    {
+    public function extraFields() {
         $pivot_class = ExtraFieldMorph::class;
         $pivot = app($pivot_class);
         $pivot_table = $pivot->getTable();
@@ -31,10 +30,22 @@ trait HasExtraFields
         ;
     }
 
-    public function extraFieldGroups()
-    {
+    public function extraFieldGroups() {
         // return $this->hasManyDeep(ExtraFieldGroup::class, [ExtraField::class, PermUser::class]);
 
-        return $this->hasManyDeepFromRelations($this->extraFields(), (new Extrafield())->group());
+        // return $this->hasManyDeepFromRelations($this->extraFields(), (new Extrafield())->group());
+
+        $pivot_class = ExtraFieldGroupMorph::class;
+        $pivot = app($pivot_class);
+        $pivot_table = $pivot->getTable();
+        $pivot_fields = $pivot->getFillable();
+
+        // dddx($pivot_fields);
+
+        return $this->morphToMany(ExtraFieldGroup::class, 'model', $pivot_table)
+            ->using($pivot_class)
+            ->withPivot($pivot_fields)
+            // ->withTimestamps()
+        ;
     }
 }
