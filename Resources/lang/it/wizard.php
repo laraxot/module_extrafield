98873fe96fdfcade75b2373115297b38978ab984
marco76tv <marco76tv@users.noreply.github.com>
2023-05-10 14:22:17 +0000

    Lint

--- Resources/lang/it/wizard.php
+++ Resources/lang/it/wizard.php
@@ -1,32 +1,28 @@
-<?php return array (
-  'modal' => 
-  array (
-    'extra-field-group' => 
-    array (
-      'add' => 'Aggiungi Campo',
-    ),
-  ),
-  'extra-field-group' => 
-  array (
-    'steps' => 
-    array (
-      'select-group-step' => 'Gruppo',
-      'insert-data-step' => 'Dati',
-    ),
-    'add' => 
-    array (
-      'select-group-step' => 
-      array (
-        'label' => 'Seleziona un gruppo',
-      ),
-      'insert-data-step' => 
-      array (
-        'label' => 'Inserisci i tuoi dati',
-      ),
-      'insert-description-step' => 
-      array (
-        'label' => 'Inserisci Note',
-      ),
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'modal' => [
+        'extra-field-group' => [
+            'add' => 'Aggiungi Campo',
+        ],
+    ],
+    'extra-field-group' => [
+        'steps' => [
+            'select-group-step' => 'Gruppo',
+            'insert-data-step' => 'Dati',
+        ],
+        'add' => [
+            'select-group-step' => [
+                'label' => 'Seleziona un gruppo',
+            ],
+            'insert-data-step' => [
+                'label' => 'Inserisci i tuoi dati',
+            ],
+            'insert-description-step' => [
+                'label' => 'Inserisci Note',
+            ],
+        ],
+    ],
+];
