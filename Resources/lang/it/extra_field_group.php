28e35578228e1d3853b602c18678ba2702340012
marco76tv <marco.sottana@gmail.com>
2023-05-26 10:54:47 +0200

    up

--- Resources/lang/it/extra_field_group.php
+++ Resources/lang/it/extra_field_group.php
@@ -12,5 +12,12 @@ return [
             'label' => 'Cardinalità',
             'placeholder' => 'Inserisci la cardinalità del dato composto',
         ],
+        'can_verified' => [
+            'label' => 'Può essere verificato',
+        ],
+        'verified_by' => [
+            'label' => 'Verificabile tramite',
+            'placeholder' => 'Seleziona la modalità di verifica',
+        ],
     ],
 ];
