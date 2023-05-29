00edace4baf7e74be13601788bee74c54c17eda8
marco xot <marco.sottana@gmail.com>
2023-05-29 16:40:22 +0200

    up

--- Actions/ExtraFieldGroup/GetByModelUserIdCategoryId.php
+++ Actions/ExtraFieldGroup/GetByModelUserIdCategoryId.php
@@ -8,12 +8,10 @@ use Illuminate\Database\Eloquent\Collection;
 use Modules\ExtraField\Models\Contracts\HasExtraFieldGroupsContract;
 use Spatie\QueueableAction\QueueableAction;
 
-class GetByModelUserIdCategoryId
-{
+class GetByModelUserIdCategoryId {
     use QueueableAction;
 
-    public function execute(HasExtraFieldGroupsContract $model, string $user_id, string $cat_id): Collection
-    {
+    public function execute(HasExtraFieldGroupsContract $model, string $user_id, string $cat_id): Collection {
         // Call to an undefined method Illuminate\Database\Eloquent\Relations\MorphToMany::withAnyCategories().
         $res = $model
             ->extraFieldGroups()
@@ -21,9 +19,6 @@ class GetByModelUserIdCategoryId
             ->wherePivot('user_id', $user_id)
             ->wherePivot('uuid', '!=', '')
             ->get();
-
-        dddx(rowsToSql($model
-        ->extraFieldGroups()));
 
         return $res;
     }
